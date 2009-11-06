fe247ed87c17a5ea57c734a2451f9eebd2ad113a
esmasui@gmail.com <esmasui@gmail.com@f6300c86-c8c0-11de-8f3f-2f730f80caef>
2009-11-06 09:18:56 +0000

    implements enable activity.

--- backport-android-bluetooth/src/backport/com/android/settings/bluetooth/RequestEnableActivity.java
+++ backport-android-bluetooth/src/backport/com/android/settings/bluetooth/RequestEnableActivity.java
@@ -1,15 +1,23 @@
 package backport.com.android.settings.bluetooth;
 
-import backport.android.bluetooth.R;
 import android.os.Bundle;
-
+import android.view.View;
+import backport.android.bluetooth.BluetoothAdapter;
+import backport.android.bluetooth.R;
 
 public class RequestEnableActivity extends RequestPermissionActivity {
-	
+
+	private BluetoothAdapter _bluetooth = BluetoothAdapter.getDefaultAdapter();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-	
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.enable);
+	}
+
+	public void onButtonClicked(View view) {
+
+		_bluetooth.enable();
 	}
 }
